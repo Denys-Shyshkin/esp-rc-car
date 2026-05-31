2aac0e283295a1f223001e30dddb00c96fa64dda
Denys Shyshkin <denys.shyshkin@yahoo.com>
2026-05-31 19:25:06 +0300

    turns and slow braking added

--- src/main.cpp
+++ src/main.cpp
@@ -16,13 +16,24 @@
 #define RESOLUTION 8
 #define MAX_DUTY 255
 
-#define MAX_CONTROL_VALUE 8
+#define MAX_SPEED_PERCENTAGE 0.8 // 1 is max (100%)
+#define MAX_CONTROL_VALUE 7
+
+const int set_control_value = MAX_CONTROL_VALUE / MAX_SPEED_PERCENTAGE;
+
+int prev_speed = 0;
+char* direction_name = "STOP";
 
 void init_logs();
+
 void adjust_speed(int speed);
+void slow_braking(int speed);
 void full_stop();
+
 void move_forward(int speed);
 void move_backward(int speed);
+void turn_right(int speed);
+void turn_left(int speed);
 
 void setup() {
     init_logs();
@@ -49,21 +60,42 @@ void loop() {
   Dabble.processInput();
 
   int direction = GamePad.getAngle(); // 60-120 forward, 240-300 backward
-  int speed = (MAX_DUTY / MAX_CONTROL_VALUE) * GamePad.getRadius();
+  int radius = GamePad.getRadius(); // 0-7
+  int speed = radius != 0 ? (MAX_DUTY / set_control_value) * radius : prev_speed;
+  
+  if (radius > 0) {
+    if (direction >= 60 && direction <= 120) {
+      move_forward(speed);
+      direction_name= "FORWARD";
+    }
+  
+    if (direction >= 150 && direction <= 210) {
+      turn_right(speed);
+      direction_name= "RIGHT";
+    }
+  
+    if (direction >= 240 && direction <= 300) {
+      move_backward(speed);
+      direction_name= "BACKWARD";
+    }
+  
+    if (direction >= 330 && direction <= 360 || direction >= 0 && direction <= 30) {
+      turn_left(speed);
+      direction_name= "LEFT";
+    }
+  } else if (prev_speed != 0) {
+    slow_braking(speed);
+    direction_name= "STOP";
 
-  if (direction >= 60 && direction <= 120) {
-    move_forward(speed);
-    Serial.println("Forward");
+    speed = 0;
   }
 
-  if (direction >= 240 && direction <= 300) {
-    move_backward(speed);
-    Serial.println("Rear");
-  }
+  Serial.print("Speed: ");
+  Serial.print(speed);
+  Serial.print(" | Direction: ");
+  Serial.println(direction_name);
 
-  if (direction == 0) {
-    full_stop();
-  }
+  prev_speed = speed;
 
   delay(20); 
 }
@@ -78,6 +110,18 @@ void init_logs() {
 void adjust_speed(int speed) {
   ledcWrite(CHANNEL_A, speed);
   ledcWrite(CHANNEL_B, speed);
+}
+
+void slow_braking(int speed) {
+  for (int i = speed; i > 0; i -= 2) {
+    adjust_speed(i);
+    
+    Serial.print("Speed: ");
+    Serial.print(i);
+    Serial.println(" | Direction: BRAKING");
+
+    delay(10);
+  }
 }
 
 void full_stop() {
@@ -101,6 +145,26 @@ void move_backward(int speed) {
 
   digitalWrite(B_IN_1, LOW);
   digitalWrite(B_IN_2, HIGH);
+
+  adjust_speed(speed);
+}
+
+void turn_right(int speed) {
+  digitalWrite(A_IN_1, HIGH);
+  digitalWrite(A_IN_2, LOW);
+
+  digitalWrite(B_IN_1, LOW);
+  digitalWrite(B_IN_2, HIGH);
+
+  adjust_speed(speed);
+}
+
+void turn_left(int speed) {
+  digitalWrite(A_IN_1, LOW);
+  digitalWrite(A_IN_2, HIGH);
+
+  digitalWrite(B_IN_1, HIGH);
+  digitalWrite(B_IN_2, LOW);
 
   adjust_speed(speed);
 }
